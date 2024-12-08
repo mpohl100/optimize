6d72be0c52be301b5748104c0bde295af2627c7a
Michael Pohl <mpohl100@googlemail.com>
2024-12-08 08:53:23 +0000

    remove redundant shape state of phenotype

--- src/gen/pheno/nn_pheno.rs
+++ src/gen/pheno/nn_pheno.rs
@@ -2,20 +2,17 @@ use super::nn_mutater::NeuralNetworkMutater;
 use crate::evol::phenotype::Phenotype;
 use crate::evol::rng::RandomNumberGenerator;
 use crate::neural::nn::neuralnet::NeuralNetwork;
-use crate::neural::nn::shape::NeuralNetworkShape;
 
 use std::sync::{Arc, Mutex};
 
 #[derive(Debug)]
 pub struct NeuralNetworkPhenotype {
-    nn_shape: NeuralNetworkShape,
     nn: Arc<Mutex<NeuralNetwork>>,
 }
 
 impl Clone for NeuralNetworkPhenotype {
     fn clone(&self) -> Self {
         Self {
-            nn_shape: self.nn_shape.clone(),
             nn: Arc::new(Mutex::new(self.get_nn())),
         }
     }
@@ -23,9 +20,7 @@ impl Clone for NeuralNetworkPhenotype {
 
 impl NeuralNetworkPhenotype {
     pub fn new(nn: NeuralNetwork) -> Self {
-        let nn_shape = nn.shape();
         Self {
-            nn_shape: nn_shape.clone(),
             nn: Arc::new(Mutex::new(nn)),
         }
     }
@@ -36,24 +31,19 @@ impl NeuralNetworkPhenotype {
 
     pub fn set_nn(&mut self, nn: NeuralNetwork) {
         *self.nn.lock().unwrap() = nn;
-        self.deduce_shape();
-    }
-
-    fn deduce_shape(&mut self) {
-        self.nn_shape = self.get_nn().shape().clone();
     }
 }
 
 impl Phenotype for NeuralNetworkPhenotype {
     fn crossover(&mut self, other: &Self) {
-        let left_half_shape = self
-            .nn_shape
+        let left_original_nn = self.get_nn();
+        let right_original_nn = other.get_nn();
+        let left_half_shape = left_original_nn.shape()
             .clone()
-            .cut_out(0, self.nn_shape.num_layers() / 2);
-        let right_half_shape = other
-            .nn_shape
+            .cut_out(0, left_original_nn.shape().num_layers() / 2);
+        let right_half_shape = right_original_nn.shape()
             .clone()
-            .cut_out(other.nn_shape.num_layers() / 2, other.nn_shape.num_layers());
+            .cut_out(right_original_nn.shape().num_layers() / 2, right_original_nn.shape().num_layers());
         let left_nn = self
             .get_nn()
             .get_subnetwork(left_half_shape)
@@ -68,7 +58,7 @@ impl Phenotype for NeuralNetworkPhenotype {
 
     fn mutate(&mut self, rng: &mut RandomNumberGenerator) {
         let mut mutater = NeuralNetworkMutater::new(rng);
-        let mutated_shape = mutater.mutate_shape(self.nn_shape.clone());
+        let mutated_shape = mutater.mutate_shape(self.get_nn().shape().clone());
         let mut nn = self.get_nn();
         nn.adapt_to_shape(mutated_shape);
         self.set_nn(nn);
