042d38cc833cdb0e58fed7cecf4cf0148e2f7b45
Michael Pohl <mpohl100@googlemail.com>
2024-12-21 09:14:20 +0100

    made rng mockable in neural network mutater

---
+++ src/gen/pheno/rng_wrapper.rs
@@ -0,0 +1,45 @@
+use crate::evol::rng::RandomNumberGenerator;
+
+use std::collections::VecDeque;
+
+pub trait RngWrapper{
+    fn fetch_uniform(&mut self, min: f32, max: f32, count: usize) -> VecDeque<f32>;
+}
+
+pub struct RealRng<'a> {
+    rng: &'a mut RandomNumberGenerator,
+}
+
+impl<'a> RealRng<'a> {
+    pub fn new(rng: &'a mut RandomNumberGenerator) -> Self {
+        RealRng { rng }
+    }
+}
+
+impl<'a> RngWrapper for RealRng<'a> {
+    fn fetch_uniform(&mut self, min: f32, max: f32, count: usize) -> VecDeque<f32> {
+        self.rng.fetch_uniform(min, max, count)
+    }
+}
+
+pub struct FakeRng {
+    values: VecDeque<f32>,
+}
+
+impl FakeRng {
+    pub fn new(values: Vec<f32>) -> Self {
+        FakeRng {
+            values: values.into_iter().collect(),
+        }
+    }
+}
+
+impl RngWrapper for FakeRng {
+    fn fetch_uniform(&mut self, _min: f32, _max: f32, count: usize) -> VecDeque<f32> {
+        let mut result = VecDeque::new();
+        for _ in 0..count {
+            result.push_back(self.values.pop_front().unwrap());
+        }
+        result
+    }
+}
