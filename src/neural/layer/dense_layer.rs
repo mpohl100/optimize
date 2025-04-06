241b3d83dfaeff32f53e5968f54f5d2cb184b483
Michael Pohl <mpohl100@googlemail.com>
2025-04-06 14:09:17 +0000

    pass directory to constructions of all layers

--- src/neural/layer/dense_layer.rs
+++ src/neural/layer/dense_layer.rs
@@ -653,7 +653,7 @@ mod tests {
 
     #[test]
     fn test_dense_layer() {
-        let mut layer = TrainableDenseLayer::new(3, 2);
+        let mut layer = TrainableDenseLayer::new(3, 2, "test_model".to_string(), 0);
 
         let input = vec![1.0, 2.0, 3.0];
         layer.allocate();
