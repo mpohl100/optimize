d2eacf60b2bda10098c7c20db4b086f6386d8cf7
Michael Pohl <mpohl100@googlemail.com>
2025-04-21 21:21:20 +0000

    delete dead code. remove unnecessary functions from traits

--- src/neural/nn/nn_trait.rs
+++ src/neural/nn/nn_trait.rs
@@ -5,24 +5,12 @@ pub trait NeuralNetwork: Clone {
     fn predict(&mut self, input: Vec<f64>) -> Vec<f64>;
     /// Creates a new `NeuralNetwork` from the given model directory.
     fn from_disk(model_directory: String) -> Option<Self>;
-
-    /// Performs a forward pass through the network with the given input.
-    fn forward(&mut self, input: &[f64]) -> Vec<f64>;
-
-    /// Performs a forward pass through the network with the given input doing batch caching.
-    fn forward_batch(&mut self, input: &[f64]) -> Vec<f64>;
     fn shape(&self) -> &NeuralNetworkShape;
     fn save(&mut self, user_model_directory: String) -> Result<(), Box<dyn std::error::Error>>;
     fn get_model_directory(&self) -> Directory;
 }
 
 pub trait TrainableNeuralNetwork: NeuralNetwork {
-    /// Performs a backward pass through the network with the given output gradient.
-    fn backward(&mut self, grad_output: Vec<f64>);
-
-    /// Performs a backward pass through the network with the given output gradient doing batch caching.
-    fn backward_batch(&mut self, grad_output: Vec<f64>);
-
     /// Trains the neural network using the given inputs, targets, learning rate, and number of epochs.
     /// Includes validation using a split of the data.
     #[allow(clippy::too_many_arguments)]
