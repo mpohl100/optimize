5ebfd43cd8969442a80130db9379cd97d6f2b569
Michael Pohl <mpohl100@googlemail.com>
2025-04-23 18:18:24 +0000

    add wrapped neural network functions

--- src/neural/nn/nn_trait.rs
+++ src/neural/nn/nn_trait.rs
@@ -1,16 +1,41 @@
+use dyn_clone::DynClone;
+
 use crate::neural::nn::directory::Directory;
 use crate::neural::nn::shape::NeuralNetworkShape;
+use std::sync::{Arc, Mutex};
 
-pub trait NeuralNetwork: Clone {
+pub trait NeuralNetwork: std::fmt::Debug {
     fn predict(&mut self, input: Vec<f64>) -> Vec<f64>;
-    /// Creates a new `NeuralNetwork` from the given model directory.
-    fn from_disk(model_directory: String) -> Option<Self>;
-    fn shape(&self) -> &NeuralNetworkShape;
+    fn shape(&self) -> NeuralNetworkShape;
     fn save(&mut self, user_model_directory: String) -> Result<(), Box<dyn std::error::Error>>;
     fn get_model_directory(&self) -> Directory;
 }
 
-pub trait TrainableNeuralNetwork: NeuralNetwork {
+pub struct WrappedNeuralNetwork {
+    nn: Arc<Mutex<Box<dyn NeuralNetwork + Send>>>,
+}
+
+impl WrappedNeuralNetwork {
+    pub fn new(nn: Box<dyn NeuralNetwork + Send>) -> Self {
+        Self {
+            nn: Arc::new(Mutex::new(nn)),
+        }
+    }
+
+    pub fn predict(&mut self, input: Vec<f64>) -> Vec<f64> {
+        self.nn.lock().unwrap().predict(input)
+    }
+
+    pub fn shape(&self) -> NeuralNetworkShape {
+        self.nn.lock().unwrap().shape()
+    }
+
+    pub fn save(&mut self, user_model_directory: String) -> Result<(), Box<dyn std::error::Error>> {
+        self.nn.lock().unwrap().save(user_model_directory)
+    }
+}
+
+pub trait TrainableNeuralNetwork: NeuralNetwork + DynClone {
     /// Trains the neural network using the given inputs, targets, learning rate, and number of epochs.
     /// Includes validation using a split of the data.
     #[allow(clippy::too_many_arguments)]
@@ -41,4 +66,84 @@ pub trait TrainableNeuralNetwork: NeuralNetwork {
 
     /// Returns the output size of the last layer in the network.
     fn output_size(&self) -> usize;
+}
+
+dyn_clone::clone_trait_object!(TrainableNeuralNetwork);
+
+pub struct WrappedTrainableNeuralNetwork {
+    nn: Arc<Mutex<Box<dyn TrainableNeuralNetwork + Send>>>,
+}
+
+impl WrappedTrainableNeuralNetwork {
+    pub fn new(nn: Box<dyn TrainableNeuralNetwork + Send>) -> Self {
+        Self {
+            nn: Arc::new(Mutex::new(nn)),
+        }
+    }
+
+    pub fn predict(&mut self, input: Vec<f64>) -> Vec<f64> {
+        self.nn.lock().unwrap().predict(input)
+    }
+
+    pub fn shape(&self) -> NeuralNetworkShape {
+        self.nn.lock().unwrap().shape()
+    }
+
+    pub fn save(&mut self, user_model_directory: String) -> Result<(), Box<dyn std::error::Error>> {
+        self.nn.lock().unwrap().save(user_model_directory)
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub fn train(
+        &mut self,
+        inputs: &[Vec<f64>],
+        targets: &[Vec<f64>],
+        learning_rate: f64,
+        epochs: usize,
+        tolerance: f64,
+        use_adam: bool,
+        validation_split: f64,
+    ) {
+        self.nn.lock().unwrap().train(
+            inputs,
+            targets,
+            learning_rate,
+            epochs,
+            tolerance,
+            use_adam,
+            validation_split,
+        )
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub fn train_batch(
+        &mut self,
+        inputs: &[Vec<f64>],
+        targets: &[Vec<f64>],
+        learning_rate: f64,
+        epochs: usize,
+        tolerance: f64,
+        batch_size: usize,
+    ) {
+        self.nn.lock().unwrap().train_batch(
+            inputs,
+            targets,
+            learning_rate,
+            epochs,
+            tolerance,
+            batch_size,
+        )
+    }
+
+    pub fn input_size(&self) -> usize {
+        self.nn.lock().unwrap().input_size()
+    }
+
+    pub fn output_size(&self) -> usize {
+        self.nn.lock().unwrap().output_size()
+    }
+
+    pub fn get_model_directory(&self) -> Directory {
+        self.nn.lock().unwrap().get_model_directory()
+    }
 }
