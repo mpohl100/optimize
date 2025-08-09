4e7aaf5341f442b9b361da07287ebdced8d29771
Michael Pohl <mpohl100@googlemail.com>
2025-08-09 15:01:53 +0000

    implemented roshambo solver

--- src/regret/regret_node.rs
+++ src/regret/regret_node.rs
@@ -1,61 +1,78 @@
 use std::sync::{Arc, Mutex};
 
-pub trait ChildrenProvider<UserData: Clone> {
-    fn get_children(&self) -> Vec<WrappedRegretNode<UserData>>;
+pub trait UserDataTrait: Clone {
+    // Define the methods that UserData should implement
+    fn get_probability(&self) -> f64;
+}
+
+pub trait ChildrenProvider<UserData: UserDataTrait> {
+    fn get_children(
+        &self,
+        parents_data: Vec<UserData>,
+    ) -> Vec<WrappedRegretNode<UserData>>;
 }
 
 #[derive(Clone)]
-pub struct WrappedChildrenProvider<UserData> {
+pub struct WrappedChildrenProvider<UserData: UserDataTrait> {
     provider: Arc<Mutex<Box<dyn ChildrenProvider<UserData>>>>,
 }
 
-impl<UserData: Clone> WrappedChildrenProvider<UserData> {
+impl<UserData: UserDataTrait> WrappedChildrenProvider<UserData> {
     pub fn new(provider: Box<dyn ChildrenProvider<UserData>>) -> Self {
         WrappedChildrenProvider { provider: Arc::new(Mutex::new(provider)) }
     }
 
-    pub fn get_children(&self) -> Vec<WrappedRegretNode<UserData>> {
-        self.provider.lock().unwrap().get_children()
+    pub fn get_children(
+        &self,
+        parents_data: Vec<UserData>,
+    ) -> Vec<WrappedRegretNode<UserData>> {
+        self.provider.lock().unwrap().get_children(parents_data)
     }
 }
 
-pub trait ExpectedValueProvider {
-    fn get_expected_value(&self) -> f64;
+pub trait ExpectedValueProvider<UserData: UserDataTrait> {
+    fn get_expected_value(
+        &self,
+        parents_data: Vec<UserData>,
+    ) -> f64;
 }
 
 #[derive(Clone)]
-pub struct WrappedExpectedValueProvider {
-    provider: Arc<Mutex<Box<dyn ExpectedValueProvider>>>,
+pub struct WrappedExpectedValueProvider<UserData: UserDataTrait> {
+    provider: Arc<Mutex<Box<dyn ExpectedValueProvider<UserData>>>>,
 }
 
-impl WrappedExpectedValueProvider {
-    pub fn new(provider: Box<dyn ExpectedValueProvider>) -> Self {
+impl<UserData: UserDataTrait> WrappedExpectedValueProvider<UserData> {
+    pub fn new(provider: Box<dyn ExpectedValueProvider<UserData>>) -> Self {
         WrappedExpectedValueProvider { provider: Arc::new(Mutex::new(provider)) }
     }
 
-    pub fn get_expected_value(&self) -> f64 {
-        self.provider.lock().unwrap().get_expected_value()
+    pub fn get_expected_value(
+        &self,
+        parents_data: Vec<UserData>,
+    ) -> f64 {
+        self.provider.lock().unwrap().get_expected_value(parents_data)
     }
 }
 
 #[derive(Clone)]
-pub enum ProviderType<UserData: Clone> {
+pub enum ProviderType<UserData: UserDataTrait> {
     Children(WrappedChildrenProvider<UserData>),
-    ExpectedValue(WrappedExpectedValueProvider),
+    ExpectedValue(WrappedExpectedValueProvider<UserData>),
 }
 
 #[derive(Clone)]
-pub struct Provider<UserData: Clone> {
+pub struct Provider<UserData: UserDataTrait> {
     pub provider_type: ProviderType<UserData>,
-    pub user_data: UserData,
+    pub user_data: Option<UserData>,
 }
 
 #[derive(Clone)]
-pub struct RegretNode<UserData: Clone> {
+pub struct RegretNode<UserData: UserDataTrait> {
     probability: f64,
     min_probability: f64,
     current_expected_value: f64,
-    parent: Option<WrappedRegretNode<UserData>>,
+    parents_data: Vec<UserData>,
     provider: Provider<UserData>,
     children: Vec<WrappedRegretNode<UserData>>,
     regret: f64,
@@ -68,11 +85,11 @@ pub struct RegretNode<UserData: Clone> {
     fixed_probability: Option<f64>,
 }
 
-impl<UserData: Clone> RegretNode<UserData> {
+impl<UserData: UserDataTrait> RegretNode<UserData> {
     pub fn new(
         probability: f64,
         min_probability: f64,
-        parent: Option<WrappedRegretNode<UserData>>,
+        parents_data: Vec<UserData>,
         provider: Provider<UserData>,
         fixed_probability: Option<f64>,
     ) -> Self {
@@ -80,7 +97,7 @@ impl<UserData: Clone> RegretNode<UserData> {
             probability,
             min_probability,
             current_expected_value: 0.0,
-            parent,
+            parents_data,
             children: Vec::new(),
             provider,
             regret: 0.0,
@@ -203,9 +220,11 @@ impl<UserData: Clone> RegretNode<UserData> {
         self.populate_children();
 
         self.current_expected_value = match self.provider.provider_type {
-            ProviderType::ExpectedValue(ref provider) => provider.get_expected_value(),
+            ProviderType::ExpectedValue(ref provider) => {
+                provider.get_expected_value(self.parents_data.clone())
+            },
             ProviderType::Children(ref provider) => {
-                self.children = provider.get_children();
+                self.children = provider.get_children(self.parents_data.clone());
                 self.children
                     .iter()
                     .map(|child| child.get_expected_value() * child.get_probability())
@@ -220,7 +239,8 @@ impl<UserData: Clone> RegretNode<UserData> {
     }
 
     fn get_total_probability(&self) -> f64 {
-        let parent_probability = self.parent.as_ref().map_or(1.0, |p| p.get_total_probability());
+        let parent_probability =
+            self.parents_data.iter().fold(1.0, |acc, data| acc * data.get_probability());
         parent_probability * self.probability
     }
 
@@ -231,7 +251,7 @@ impl<UserData: Clone> RegretNode<UserData> {
     fn populate_children(&mut self) {
         match self.provider.provider_type {
             ProviderType::Children(ref provider) => {
-                self.children = provider.get_children();
+                self.children = provider.get_children(self.parents_data.clone());
             },
             ProviderType::ExpectedValue(_) => {
                 // If the provider is an expected value provider, we don't need to populate children
@@ -258,11 +278,11 @@ impl<UserData: Clone> RegretNode<UserData> {
 }
 
 #[derive(Clone)]
-pub struct WrappedRegretNode<UserData: Clone> {
+pub struct WrappedRegretNode<UserData: UserDataTrait> {
     node: Arc<Mutex<RegretNode<UserData>>>,
 }
 
-impl<UserData: Clone> WrappedRegretNode<UserData> {
+impl<UserData: UserDataTrait> WrappedRegretNode<UserData> {
     pub fn new(node: RegretNode<UserData>) -> Self {
         WrappedRegretNode { node: Arc::new(Mutex::new(node)) }
     }
@@ -307,5 +327,143 @@ impl<UserData: Clone> WrappedRegretNode<UserData> {
 
     pub fn get_average_expected_value(&self) -> f64 {
         self.node.lock().unwrap().get_average_expected_value()
+    }
+}
+
+mod tests {
+    use core::panic;
+
+    use super::*;
+
+    #[derive(Clone)]
+    enum Choice {
+        Rock,
+        Paper,
+        Scissors,
+    }
+
+    #[derive(Clone)]
+    struct RoshamboData {
+        choice: Choice,
+    }
+
+    impl UserDataTrait for RoshamboData {
+        fn get_probability(&self) -> f64 {
+            match self.choice {
+                Choice::Rock => 0.33333,
+                Choice::Paper => 0.33333,
+                Choice::Scissors => 0.33334,
+            }
+        }
+    }
+
+    struct RoshamboChildrenProvider {}
+
+    impl ChildrenProvider<RoshamboData> for RoshamboChildrenProvider {
+        fn get_children(
+            &self,
+            parents_data: Vec<RoshamboData>,
+        ) -> Vec<WrappedRegretNode<RoshamboData>> {
+            if parents_data.len() < 1 {
+                let mut children = Vec::new();
+                for choice in [Choice::Rock, Choice::Paper, Choice::Scissors].iter() {
+                    let cloned_parents_data = parents_data.clone();
+                    let data = RoshamboData { choice: choice.clone() };
+                    let mut parents_data = cloned_parents_data.clone();
+                    parents_data.push(RoshamboData { choice: choice.clone() });
+                    let node = RegretNode::new(
+                        0.0,
+                        0.0,
+                        parents_data,
+                        Provider {
+                            provider_type: ProviderType::Children(WrappedChildrenProvider::new(
+                                Box::new(RoshamboChildrenProvider {}),
+                            )),
+                            user_data: Some(data.clone()),
+                        },
+                        None,
+                    );
+                    children.push(WrappedRegretNode::new(node));
+                }
+                return children;
+            } else if parents_data.len() == 1 {
+                let mut children = Vec::new();
+                for choice in [Choice::Rock, Choice::Paper, Choice::Scissors].iter() {
+                    let mut parents_data_clone = parents_data.clone();
+                    let data = RoshamboData { choice: choice.clone() };
+                    parents_data_clone.push(data.clone());
+                    let node = RegretNode::new(
+                        0.0,
+                        0.0,
+                        parents_data_clone,
+                        Provider {
+                            provider_type: ProviderType::ExpectedValue(
+                                WrappedExpectedValueProvider::new(Box::new(
+                                    RoshamboExpectedValueProvider {},
+                                )),
+                            ),
+                            user_data: Some(data.clone()),
+                        },
+                        None,
+                    );
+                    children.push(WrappedRegretNode::new(node));
+                }
+                return children;
+            }
+            return Vec::new();
+        }
+    }
+
+    struct RoshamboExpectedValueProvider {}
+
+    impl ExpectedValueProvider<RoshamboData> for RoshamboExpectedValueProvider {
+        fn get_expected_value(
+            &self,
+            parents_data: Vec<RoshamboData>,
+        ) -> f64 {
+            if parents_data.len() < 2 {
+                panic!("Expected at least two parents data for expected value calculation");
+            }
+            let player_1_choice = &parents_data[0].choice;
+            let player_2_choice = &parents_data[1].choice;
+            match player_1_choice {
+                Choice::Rock => match player_2_choice {
+                    Choice::Rock => 0.0,     // Tie
+                    Choice::Paper => -1.0,   // Paper beats Rock
+                    Choice::Scissors => 1.0, // Rock beats Scissors
+                },
+                Choice::Paper => match player_2_choice {
+                    Choice::Rock => 1.0,      // Paper beats Rock
+                    Choice::Paper => 0.0,     // Tie
+                    Choice::Scissors => -1.0, // Scissors beats Paper
+                },
+                Choice::Scissors => match player_2_choice {
+                    Choice::Rock => -1.0,    // Rock beats Scissors
+                    Choice::Paper => 1.0,    // Paper beats Rock
+                    Choice::Scissors => 0.0, // Tie
+                },
+            }
+        }
+    }
+
+    #[test]
+    fn test_roshambo_regret_minimization() {
+        let mut node = RegretNode::new(
+            0.0,
+            0.0,
+            vec![],
+            Provider {
+                provider_type: ProviderType::Children(WrappedChildrenProvider::new(Box::new(
+                    RoshamboChildrenProvider {},
+                ))),
+                user_data: None,
+            },
+            Some(1.0),
+        );
+
+        node.solve(1000);
+
+        assert!(node.get_average_probability() > 0.0);
+        assert!(node.get_average_expected_value() != 0.0);
     }
 }
