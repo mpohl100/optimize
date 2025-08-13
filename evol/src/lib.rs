a55ccdfd08aac36835942ef507d34fc9e04289c2
Michael Pohl <mpohl100@googlemail.com>
2025-08-13 13:06:57 +0000

    added clippy stuff to all lib.rs

--- evol/src/lib.rs
+++ evol/src/lib.rs
@@ -1,3 +1,10 @@
+#![warn(clippy::all)]
+#![warn(clippy::style)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::multiple_crate_versions)]
 pub mod evolution;
 pub mod phenotype;
 pub mod rng;
--- neural/src/lib.rs
+++ neural/src/lib.rs
@@ -1,3 +1,10 @@
+#![warn(clippy::all)]
+#![warn(clippy::style)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::multiple_crate_versions)]
 pub mod activation;
 pub mod layer;
 pub mod mat;
