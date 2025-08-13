2bc14b3cf449d2c74a9bcca805f2683ef67449dd
Michael Pohl <mpohl100@googlemail.com>
2025-08-13 13:06:57 +0000

    added clippy stuff to all lib.rs

--- gen/src/lib.rs
+++ gen/src/lib.rs
@@ -1,3 +1,10 @@
+#![warn(clippy::all)]
+#![warn(clippy::style)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::multiple_crate_versions)]
 pub mod challenge;
 pub mod neuralnet_gen;
 pub mod pheno;
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,1 +1,8 @@
+#![warn(clippy::all)]
+#![warn(clippy::style)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::multiple_crate_versions)]
 pub mod safer;
