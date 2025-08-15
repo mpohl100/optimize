8a2c5310800b934fc9224e5d43320942e567790a
Michael Pohl <mpohl100@googlemail.com>
2025-08-15 14:09:10 +0000

    added integration test to use sum matrix

---
+++ matrix/tests/test_sum_mat.rs
@@ -0,0 +1,20 @@
+use matrix::mat::WrappedMatrix;
+use matrix::sum_mat::SumMatrix;
+
+#[test]
+fn test_integration_sum_matrices() {
+    let matrix = WrappedMatrix::new(3, 3);
+    let mut sum_matrix = SumMatrix::new(matrix);
+
+    sum_matrix.set_val(0, 0, 1).unwrap();
+
+    sum_matrix.set_val_unchecked(0, 1, 2);
+
+    assert_eq!(sum_matrix.get_row_sum(0), 3);
+    assert_eq!(sum_matrix.get_val(0, 0).unwrap(), 1);
+    assert_eq!(sum_matrix.get_val(0, 1).unwrap(), 2);
+    // assert get_ratio
+    let epsilon = 1e-6;
+    assert!((sum_matrix.get_ratio(0, 0).unwrap() - 1.0 / 3.0).abs() < epsilon);
+    assert!((sum_matrix.get_ratio(0, 1).unwrap() - 2.0 / 3.0).abs() < epsilon);
+}
