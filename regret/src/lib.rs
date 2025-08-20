c1c8bf0489705bbf5666f48cc0774b2715a33f8d
Michael Pohl <mpohl100@googlemail.com>
2025-08-20 14:42:05 +0000

    add github copilot chat and linter warnings

--- regret/src/lib.rs
+++ regret/src/lib.rs
@@ -5,4 +5,10 @@
 #![warn(clippy::cargo)]
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::multiple_crate_versions)]
+#![warn(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc
+)]
 pub mod solver_node;
