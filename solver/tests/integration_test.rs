331e85dab4c31946393d3cf9285e0d144fb45f46
Michael Pohl <mpohl100@googlemail.com>
2026-02-21 14:17:45 +0000

    got solver to build

--- solver/tests/integration_test.rs
+++ solver/tests/integration_test.rs
@@ -4,6 +4,7 @@ use neural::layer::dense_layer::MatrixParams;
 use neural::nn::shape::{
     ActivationData, ActivationType, LayerShape, LayerType, NeuralNetworkShape,
 };
+use neural::training::training_params::TrainingParams;
 use solver::neural_solver::NeuralSolver;
 
 #[test]
@@ -29,8 +30,32 @@ fn test_neural_solver_creation() {
     ];
     let shape = NeuralNetworkShape::new(layers);
 
+    let levels = None;
+    let pre_shape = None;
+    let validation_split = 1.0;
+    let learning_rate = 0.01;
+    let epochs = 10;
+    let tolerance = 0.001;
+    let batch_size = 32;
+    let use_adam = true;
+    let sample_match_percentage = 0.9;
+
+    let training_params = TrainingParams::new(
+        shape.clone(),
+        levels,
+        pre_shape,
+        validation_split,
+        learning_rate,
+        epochs,
+        tolerance,
+        batch_size,
+        use_adam,
+        sample_match_percentage,
+    );
+    let all_inputs = vec![vec![0.0; 10]; 100];
+    let all_targets = vec![vec![0.0; 3]; 100];
     // Create a neural solver with the shape
-    let mut solver = NeuralSolver::new(shape);
+    let mut solver = NeuralSolver::new(shape, training_params, all_inputs, all_targets);
 
     // Call the solve method (with minimal iterations for testing)
     solver.solve(1, false);
